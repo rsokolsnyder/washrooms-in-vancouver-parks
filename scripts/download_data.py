da291c68f0f0ed2c0f839af33d1a33ecbbb626d0
Harry Yau <yauchungki1305@gmail.com>
2025-12-10 22:15:11 -0800

    function for download data

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -5,15 +5,24 @@
 import click
 import pandas as pd
 import os
+
+def download_and_save_csv(url: str, write_to: str, filename: str = "parks.csv"):
+    """Download a CSV file from a URL and save it to local directory."""
+    try:
+        df = pd.read_csv(url, sep=";")
+    except Exception as e:
+        raise Exception("File appears to be in the incorrect format!")
+    
+    output_path = os.path.join(write_to, filename)
+    df.to_csv(output_path, index=False)
+    return output_path
+    
 @click.command()
 @click.option('--url', type=str, help="URL of dataset to be download")
 @click.option('--write_to', type=str, help="Path for the saving location")
 def main(url, write_to):
     """Download data from the website to local directory with the path."""
-    try:
-        park = pd.read_csv(url, sep=";")
-    except:
-        raise Exception("File appears to be in the incorrect format!")
-    park.to_csv(os.path.join(write_to, "parks.csv"), index=False)
+    download_and_save_csv(url, write_to)
+    
 if __name__ == '__main__':
     main()
